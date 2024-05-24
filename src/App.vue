5b5a69854e428db51e1a0937a94282661ec9a8ac
Branislav Juhás <juhas.branislaw@gmail.com>
2024-05-24 22:17:51 +0200

    Animations Added 240524

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,35 @@ import Header from "./components/Header.vue";
   <div
     class="grid grid-cols-1 grid-rows-[auto_1fr_auto] w-full h-full sm:grid-rows-[1fr_auto]">
     <Header class="sm:hidden" />
-    <div class="flex flex-col items-center px-[20px]">
+    <div class="flex flex-col items-center px-[20px] overflow-hidden">
       <router-view
-        class="max-w-[1440px] pt-[10rem] h-full w-full overflow-hidden" />
+        v-slot="{ Component }"
+        class="max-w-[1440px] pt-[10rem] h-full w-full overflow-hidden relative">
+        <transition name="slide-fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </div>
     <Footer />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition:
+    transform 0.21s,
+    opacity 0.21s ease;
+  position: relative;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateY(10%);
+}
+.slide-fade-enter-from,
+.slide-fade-leave {
+  opacity: 0;
+  transform: translateY(0%);
+}
+</style>
