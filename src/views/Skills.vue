7617d76ecb9312630194310f0d1e860f7fa7da51
Branislav Juhás <juhas.branislaw@gmail.com>
2024-06-17 12:33:46 +0200

    Skills Chips Completion 170624

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -25,23 +25,47 @@ let dropdown = ref(false);
     <h1 class="text-5xl font-bold">What do I do?</h1>
     <div class="relative mt-6">
       <div
-        class="relative grid grid-cols-[1.25rem_1fr_1.25rem] gap-3 z-10 items-center justify-center px-[0.88rem] py-[0.35rem] bg-yellow text-black rounded-2xl border-black border-2 hover:bg-red dark:border-white lg:hidden"
+        class="relative grid grid-cols-[1.25rem_1fr_1.25rem] gap-3 z-10 items-center justify-center px-[0.88rem] py-[0.35rem] bg-yellow text-black rounded-2xl border-black border-2 hover:bg-red dark:border-white xl:hidden"
         @click="dropdown = !dropdown">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20"
+          width="21"
           height="20"
-          viewBox="0 0 20 20"
+          viewBox="0 0 21 20"
           fill="none"
           class="w-5"
           v-html="selected.icon" />
         <p>{{ selected.name }}</p>
         <img src="../assets/icons/CaretDown-r.svg" alt="Down" class="w-5" />
       </div>
+      <div class="hidden w-full xl:flex flex-row gap-4 mt-3">
+        <div
+          v-for="skill in data.skills"
+          class="chip grid grid-cols-[1.25rem_1fr] gap-3 items-center justify-center w-full min-h-16 px-[0.88rem] py-[0.35rem] text-black border-black dark:border-white border-2 rounded-2xl bg-white dark:bg-blue-light hover:bg-green dark:hover:bg-white cursor-pointer"
+          :style="
+            selected.name === skill.name ? 'background-color: #f5da6c' : ''
+          "
+          @click="
+            selected = skill;
+            router.replace({
+              hash: '#' + skill.name.toLowerCase().replace(/ /g, '-'),
+            });
+          ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="20"
+            viewBox="0 0 21 20"
+            fill="none"
+            class="w-5"
+            v-html="skill.icon" />
+          <p class="cursor-pointer">{{ skill.name }}</p>
+        </div>
+      </div>
       <transition name="fly-fade">
         <div
           v-if="dropdown"
-          class="absolute flex flex-col gap-3 top-12 w-full px-[0.88rem] py-4 bg-yellow text-black rounded-2xl border-black border-2 dark:border-white lg:hidden">
+          class="absolute flex flex-col gap-3 top-12 w-full px-[0.88rem] py-4 bg-yellow text-black rounded-2xl border-black border-2 dark:border-white xl:hidden">
           <div
             v-for="skill in data.skills"
             :key="skill.id"
@@ -49,12 +73,15 @@ let dropdown = ref(false);
             @click="
               selected = skill;
               dropdown = false;
+              router.replace({
+                hash: '#' + skill.name.toLowerCase().replace(/ /g, '-'),
+              });
             ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
+              width="21"
               height="20"
-              viewBox="0 0 20 20"
+              viewBox="0 0 21 20"
               fill="none"
               class="w-5"
               v-html="skill.icon" />
@@ -84,5 +111,9 @@ let dropdown = ref(false);
 .fly-fade-leave {
   opacity: 0;
   transform: translateY(-20%);
+}
+
+.chip {
+  transition: background-color 0.5s ease;
 }
 </style>
