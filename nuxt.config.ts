70f4c63f24f84ef9f7897f73dbd4c22b0fd7305b
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-01 14:28:00 +0200

    chore: update ESLint configuration and enhance Nuxt.js config with Tailwind CSS support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxtjs/tailwindcss",
+  ],
+  css: ["~/assets/css/main.css"],
+  runtimeConfig: {
+    public: {
+      githubUrl: process.env.GITHUB_URL || "",
+      linkedinUrl: process.env.LINKEDIN_URL || "",
+    },
+  },
+  fonts: {
+    defaults: {
+      weights: [400, 700],
+      styles: ["normal", "italic"],
+      subsets: ["latin"],
+    },
+    provider: "google",
+    families: [
+      {
+        name: "Space Mono",
+        weights: [400, 700],
+        styles: ["normal", "italic"],
+        subsets: ["latin"],
+        preload: true,
+        provider: "google",
+      },
+    ],
+  },
+});
