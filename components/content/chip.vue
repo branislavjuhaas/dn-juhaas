f108f5c880976d274e7cfd99b0d4a3406bce36c0
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-04 13:47:19 +0200

    feat: add ContentChip component for portfolio cards

---
+++ components/content/chip.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtLink
+    v-if="props.to || props.href"
+    :to="props.to || props.href"
+    class="chip"
+    :target="props.to ? undefined : '_blank'"
+    :rel="props.to ? undefined : 'noopener noreferrer'">
+    <Icon v-if="props.icon" :name="props.icon" />
+    <slot>
+      {{ props.title }}
+    </slot>
+  </NuxtLink>
+  <span v-else class="chip">
+    <Icon v-if="props.icon" :name="props.icon" size="0.9375rem" />
+    <slot>
+      {{ props.title }}
+    </slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string;
+  icon?: string;
+  href?: string;
+  to?: string;
+}>();
+</script>
+
+<style scoped>
+@reference "./../../assets/css/main.css";
+.chip {
+  @apply flex flex-row text-xs uppercase font-bold items-center gap-1 px-2 py-0.5 rounded-[0.635rem] border-1;
+}
+</style>
---
+++ components/portfolio/card.vue
@@ -0,0 +1,51 @@
+<template>
+  <NuxtLink
+    :to="`/portfolio/${props.id}`"
+    class="flex flex-col w-full rounded-2xl p-8 border-3 border-ebony gap-4 transition-colors duration-200"
+    :class="['card-' + (props.dark ? 'dark' : 'light')]">
+    <div class="flex flex-col gap-0">
+      <h4>{{ props.title }}</h4>
+      <div class="flex flex-row gap-2">
+        <ContentChip
+          v-if="props.platform"
+          :title="props.platform"
+          icon="ph:circuitry"
+          :dark="props.dark" />
+        <ContentChip
+          v-if="props.technology"
+          :title="props.technology"
+          icon="ph:git-branch"
+          :dark="props.dark" />
+      </div>
+    </div>
+    <img
+      v-if="props.image"
+      :src="props.image"
+      alt="Project image"
+      class="w-full aspect-video object-cover rounded-[0.625rem] border-3" >
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import ContentChip from "~/components/content/chip.vue";
+
+const props = defineProps<{
+  slug: string;
+  title: string;
+  platform: string;
+  technology: string;
+  image: string;
+  dark?: boolean;
+}>();
+</script>
+
+<style scoped>
+@reference "./../../assets/css/main.css";
+
+.card-dark {
+  @apply bg-ebony text-peach hover:bg-peach hover:text-ebony;
+}
+.card-light {
+  @apply text-ebony hover:bg-ebony hover:text-peach;
+}
+</style>
