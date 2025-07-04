c5b1a972e809d0d5e70b67516d798b65dc4705cb
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-04 19:52:58 +0200

    fix: add z-index to header for improved stacking context

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header
-      class="fixed top-0 left-0 w-full h-16 px-5 bg-white flex items-center border-b-2">
+      class="fixed top-0 left-0 w-full h-16 px-5 bg-white flex items-center border-b-2 z-20">
       <div
         class="flex flex-row items-center justify-between w-full max-w-content mx-auto">
         <NuxtLink
