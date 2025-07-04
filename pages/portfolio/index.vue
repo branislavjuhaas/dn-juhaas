e8bfeb77c9301da40ba3a4e3ca9b449a303132e1
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-04 13:48:23 +0200

    feat: add portfolio page with dynamic content rendering

---
+++ pages/portfolio/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col w-full gap-8">
+    <h1>Selection of the finest work</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+      <PortfolioCard
+        v-for="(item, index) in portfolio"
+        :key="item.id"
+        :slug="item.slug"
+        :title="item.name"
+        :platform="item.platform"
+        :technology="item.technology"
+        :image="item.image"
+        :dark="index % 2 === 0" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PortfolioCard } from "#components";
+
+useHead({
+  title: "Portfolio",
+  meta: [
+    {
+      name: "description",
+      content:
+        "A selection of my finest work, showcasing my skills and projects.",
+    },
+  ],
+});
+
+// Preparation for i18n: Language selection is currently hardcoded to "sk".
+// In the future, this can be replaced with a dynamic language selection based on user preference or locale.
+const lang = "sk";
+
+const portfolio = await queryCollection(`portfolio_${lang}`)
+  .order("development", "ASC")
+  .all();
+</script>
+
+<style scoped></style>
