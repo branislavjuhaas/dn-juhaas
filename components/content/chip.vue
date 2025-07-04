d7d4d3883fb2832db664d40ca37c4ba1705deb31
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-04 19:52:37 +0200

    fix: correct class binding and enhance chip component styling

--- components/content/chip.vue
+++ components/content/chip.vue
@@ -3,14 +3,18 @@
     v-if="props.to || props.href"
     :to="props.to || props.href"
     class="chip"
-    :target="props.to ? undefined : '_blank'"
-    :rel="props.to ? undefined : 'noopener noreferrer'">
+    :class="['chip-' + (props.accent ? 'accent' : 'default')]">
+    :target="props.to ? undefined : '_blank'" :rel="props.to ? undefined :
+    'noopener noreferrer'">
     <Icon v-if="props.icon" :name="props.icon" />
     <slot>
       {{ props.title }}
     </slot>
   </NuxtLink>
-  <span v-else class="chip">
+  <span
+    v-else
+    class="chip"
+    :class="['chip-' + (props.accent ? 'accent' : 'default')]">
     <Icon v-if="props.icon" :name="props.icon" size="0.9375rem" />
     <slot>
       {{ props.title }}
@@ -24,6 +28,7 @@ const props = defineProps<{
   icon?: string;
   href?: string;
   to?: string;
+  accent?: boolean;
 }>();
 </script>
 
@@ -31,5 +36,9 @@ const props = defineProps<{
 @reference "./../../assets/css/main.css";
 .chip {
   @apply flex flex-row text-xs uppercase font-bold items-center gap-1 px-2 py-0.5 rounded-[0.635rem] border-1;
+}
+
+.chip-accent {
+  @apply bg-peach text-ebony;
 }
 </style>
