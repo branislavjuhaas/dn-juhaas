fd02649e8b149fc33db73b18e24f2313002ecffe
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-21 13:22:50 +0200

    fix: correct closing tag for img element in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -47,7 +47,7 @@
     <img
       src="./../assets/images/juhaas.webp"
       alt="Branislav's photo"
-      class="absolute aspect-square w-125 right-1/2 max-md:translate-x-3/5 bottom-0 md:right-0 -z-10 object-cover" />
+      class="absolute aspect-square w-125 right-1/2 max-md:translate-x-3/5 bottom-0 md:right-0 -z-10 object-cover" >
   </div>
 </template>
 
