99abd3281bb77ebb66712e4f03cd48f4347969f0
Branislav Juhás <juhas.branislaw@gmail.com>
2025-07-22 12:58:19 +0200

    feat: update navigation links to use localized paths

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,16 +12,16 @@
       </transition>
       <div class="flex flex-row gap-3 mt-2 max-w-fill">
         <ContentButton
-          to="/portfolio"
+          :to="$localePath('portfolio')"
           :text="$t('home.portfolio')"
           icon="ph:flag-banner-fold-fill"
           accent />
         <ContentButton
-          to="/skills"
+          :to="$localePath('skills')"
           :text="$t('home.skills')"
           icon="ph:certificate-fill" />
         <ContentButton
-          to="/about"
+          :to="$localePath('about')"
           :text="$t('home.more')"
           icon="ph:signpost-fill" />
       </div>
