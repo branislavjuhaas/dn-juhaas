be0b341ef2afa4e4789f6e47d04835f6e598f2e8
Branislav Juhás <juhas.branislaw@gmail.com>
2026-07-05 21:09:58 +0200

    feat: setup vite-plus and format

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-    github: {
-        url: "https://github.com/branislavjuhaas/dn-juhaas",
-        branch: "main",
+  github: {
+    url: "https://github.com/branislavjuhaas/dn-juhaas",
+    branch: "main",
+  },
+  ui: {
+    colors: {
+      primary: "rose",
+      neutral: "zinc",
     },
-    ui: {
-        colors: {
-            primary: "rose",
-            neutral: "zinc",
-        },
-    },
+  },
 });
