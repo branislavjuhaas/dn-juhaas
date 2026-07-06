d11924bded1a909ac42c393a6f5f120fc556cb4e
Branislav Juhás <juhas.branislaw@gmail.com>
2026-07-06 18:48:41 +0200

    feat: add links

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,4 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: "Work",
+    icon: "i-ph-swatches",
+    to: "/work",
+    children: [
+      {
+        label: "Featured",
+        description: "The most influential of recent work.",
+        icon: "i-ph-sparkle",
+      },
+      {
+        label: "Party tools",
+        description: "Collection of open source fun gadgets.",
+        icon: "i-ph-confetti",
+      },
+      {
+        label: "3rd party",
+        description: "Contributions to core open-source ecosystems.",
+        icon: "i-ph-hand-heart",
+      },
+    ],
+  },
+  {
+    label: "Stack",
+    to: "/stack",
+    icon: "i-ph-gear-six",
+    children: [
+      {
+        label: "Tools",
+        description: "The tools I use to build things.",
+        icon: "i-ph-sparkle",
+      },
+      {
+        label: "Development stack",
+        description: "The tools and libraries I use to build things.",
+        icon: "i-ph-gear-six",
+      },
+    ],
+  },
+  {
+    label: "Blog",
+    icon: "i-ph-newspaper-clipping",
+    to: "/blog",
+  },
+]);
+</script>
 
 <template>
   <UHeader :toggle="false">
@@ -8,7 +57,7 @@
       </NuxtLink>
     </template>
     <template #right>
-      <UColorModeButton />
+      <UNavigationMenu :items="items" class="w-full justify-end" />
     </template>
   </UHeader>
 </template>
