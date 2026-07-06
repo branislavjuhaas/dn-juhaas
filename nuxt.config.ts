151cc0a48533e637f049f02a9ea54970642a28c3
Branislav Juhás <juhas.branislaw@gmail.com>
2026-07-06 12:20:40 +0200

    feat: scaffold nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['docus'],
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+});
